3aa49bf64e38225bab898d17a0c3086ab9633d2d
Maxime Mourgues <maxime.mourgues@ynov.com>
2022-05-03 10:55:13 +0200

    package-lock, composer.lock, symfony.lock

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,21 +9,21 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/connexion', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
-    {
-        if ($this->getUser()) {
-            return $this->redirectToRoute('target_path');
-        }
-        $error = $authenticationUtils->getLastAuthenticationError();
-        $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+  #[Route(path: '/connexion', name: 'app_login')]
+  public function login(AuthenticationUtils $authenticationUtils): Response
+  {
+    if ($this->getUser()) {
+        return $this->redirectToRoute('app_home');
     }
+    $error = $authenticationUtils->getLastAuthenticationError();
+    $lastEmail = $authenticationUtils->getLastUsername();
 
-    #[Route(path: '/deconnexion', name: 'app_logout')]
-    public function logout(): void
-    {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
-    }
+    return $this->render('security/login.html.twig', ['last_email' => $lastEmail, 'error' => $error]);
+  }
+
+  #[Route(path: '/deconnexion', name: 'app_logout')]
+  public function logout(): void
+  {
+    throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
 	content: ["./templates/**/*.{html,js,twig}"],
 	theme: {
-		extend: {},
+		colors: {
+			'dark': {
+				'black': "#1C264A",
+				'blue': "#222E59",
+				'gray': "#324A5F"
+			},
+			'light': {
+				'blue': "#5C7389",
+				'skin': "#998287"
+			},
+			'blood': "#FF4E4B",
+			'error': '#FF0000',
+			'success': '#00FF00',
+		},
 	},
 	plugins: [],
 }
